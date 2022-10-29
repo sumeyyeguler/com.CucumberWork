b82f1b66096ae2261b5b359f1fd874bbdd7aad41
Sumeyye Guler <sumeyyeguler0602@gmail.com>
2022-10-29 13:55:48 +0300

    sumeyye

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -6,11 +6,14 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin={"html:target/cucumber-reports.html"},
+        plugin={"html:target/cucumber-reports.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml" },
         features = "src/test/resources/features",
         glue = "stepDefinitions",
-        tags = "@negativeLogin",
+        tags = "@demo", // iki tag'ı aynı anda çalıştırmak isterseniz "@gp1 or @gp2"
         dryRun = false
+
 )
 
 public class Runner {
